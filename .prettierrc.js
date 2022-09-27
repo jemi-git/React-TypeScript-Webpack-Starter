3fab2a5fa46217dfd1db1e3225b62ef1cd0f0a2a
jemi-git <114506750+jemi-git@users.noreply.github.com>
2022-09-27 21:57:37 +0530

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,3 +6,11 @@ module.exports = {
   tabWidth: 2,
   endOfLine: 'auto'
 }
+module.exports = {
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto'
+}
